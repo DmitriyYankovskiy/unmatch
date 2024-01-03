c6a03ab26196ef791830dbc4d1150136f6e47541
DmitriyYankovskiy <dmityank@gmail.com>
2024-01-03 19:19:35 +0300

    43

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, Responder, HttpServer, App, HttpResponse};
+use std::{io::Result, fs};
+// use std::fs::File::file_stream;
+
+#[get("/game")]
+async fn game() -> impl Responder {
+    match fs::read_to_string("www/game/index.html") {
+        Ok(file) => {
+            println!("{file}");
+            HttpResponse::Ok()
+            .content_type("text/html")
+            .body(file)
+        },
+        Err(..) => {
+            println!("error");
+            HttpResponse::Forbidden()
+            .body("404")
+        },
+    }
+    
+} 
+
+#[actix_web::main]
+async fn main() -> Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(game)
+    })
+    .bind(("127.0.0.1", 9999))?
+    .run()
+    .await
 }
