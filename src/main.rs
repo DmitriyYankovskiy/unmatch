4a2e7ff52a5e4a4d293bf2d05487f2963fd0691c
Alexey <yankovskyalexey123@yandex.ru>
2024-01-28 15:39:33 +0300

    first

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use actix_web::HttpRequest;
 pub use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+pub use handlebars::Handlebars;
 use reqwest::Response;
 use serde::{Serialize, Deserialize};
+use serde_json::json;
 pub use set::Set;
 use std::ops::Deref;
 pub use std::{fs, io::Result, sync::Mutex, collections::HashMap};
@@ -17,12 +19,6 @@ pub trait Readable {
         Self: std::marker::Sized;
 }
 
-#[get("/game")]
-async fn game_controller() -> impl Responder {
-    file_response("game/index.html".to_string())
-}
-
-
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 #[serde(rename_all = "camelCase")]
@@ -31,19 +27,32 @@ pub struct PlayerInfo<T> {
     info: T,
 }
 
-fn file_response(path: String) -> HttpResponse {
+fn file_response(path: String, hbs_data: web::Data<Handlebars<'_>>) -> HttpResponse {
     match fs::read_to_string(format!("www/{}", path)) {
-        Ok(file) => HttpResponse::Ok().content_type("text/html").body(file),
+        Ok(file) => HttpResponse::Ok().content_type("text/html")
+        .body(match hbs_data.render_template(&file, &json!({"title": "TITLE"})) {
+            Ok(file) => file,
+            Err(..) => return HttpResponse::Forbidden().body("403"),
+        }),
         Err(..) => HttpResponse::Forbidden().body("404"),
     }
 }
 
+
+
+// ------ Controllers ------
+
+#[get("/game")]
+async fn game_controller(hbs_data: web::Data<Handlebars<'_>>) -> impl Responder {
+    file_response("game/index.html".to_string(), hbs_data)
+}
+
 #[get("/game/connect")]
-async fn game_connect(data: web::Data<Mutex<game::GameState>>, json: web::Json<PlayerInfo<String>>) -> impl Responder {
-    let mut game = data.lock().unwrap();
+async fn game_connect(game_data: web::Data<Mutex<game::GameState>>, hbs_data: web::Data<Handlebars<'_>>, json: web::Json<PlayerInfo<String>>) -> impl Responder {
+    let mut game = game_data.lock().unwrap();
     match game.add_player(json.0) {
         Ok(id) => {
-            file_response("game/index.html".to_string())
+            file_response("game/index.html".to_string(), hbs_data)
         },
         Err(..) => {
             HttpResponse::InternalServerError().body("character not found")
@@ -53,12 +62,16 @@ async fn game_connect(data: web::Data<Mutex<game::GameState>>, json: web::Json<P
 
 #[actix_web::main]
 async fn main() -> Result<()> {
-    let mut data = web::Data::new(Mutex::new(game::GameState::new()));
+    let game_data = web::Data::new(Mutex::new(game::GameState::new()));
+    let hbs = Handlebars::new();
+    //hbs.r
+    let hbs_data: web::Data<Handlebars<'_>> = web::Data::new(hbs);
     HttpServer::new(move || 
         App::new()
-        .app_data(data.clone())
+        .app_data(game_data.clone())
+        .app_data(hbs_data.clone())
         .service(game_controller)
-    )
+    )    
     .bind(("127.0.0.1", 9999))?
     .run()
     .await
